ea7e1633d9eb7953dc701ae11edb801fb4c03739
Bit <bitlessniper@gmail.com>
2025-11-03 00:28:31 +0200

    add assets and creeper demos

---
+++ dodge_the_creeps_CS/Main.cs
@@ -0,0 +1,79 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+	[Export]
+	public PackedScene MobScene { get; set; }
+
+	private int _score;
+	
+	public void GameOver()
+	{
+		GetNode<Timer>("MobTimer").Stop();
+		GetNode<Timer>("ScoreTimer").Stop();
+		GetNode<Hud>("HUD").ShowGameOver();
+		GetNode<AudioStreamPlayer>("Music").Stop();
+		GetNode<AudioStreamPlayer>("DeathSound").Play();
+	}
+
+	public void NewGame()
+	{
+		// Note that for calling Godot-provided methods with strings,
+		// we have to use the original Godot snake_case name.
+		GetTree().CallGroup("mobs", Node.MethodName.QueueFree);
+		_score = 0;
+
+		var player = GetNode<Player>("Player");
+		var startPosition = GetNode<Marker2D>("StartPosition");
+		player.Start(startPosition.Position);
+
+		GetNode<Timer>("StartTimer").Start();
+		
+		var hud = GetNode<Hud>("HUD");
+		hud.UpdateScore(_score);
+		hud.ShowMessage("Get Ready!");
+		GetNode<AudioStreamPlayer>("Music").Play();
+	}
+	
+	private void OnMobTimerTimeout()
+	{
+		// Create a new instance of the Mob scene.
+		Mob mob = MobScene.Instantiate<Mob>();
+
+		// Choose a random location on Path2D.
+		var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+		mobSpawnLocation.ProgressRatio = GD.Randf();
+
+		// Set the mob's direction perpendicular to the path direction.
+		float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+
+		// Set the mob's position to a random location.
+		mob.Position = mobSpawnLocation.Position;
+
+		// Add some randomness to the direction.
+		direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+		mob.Rotation = direction;
+
+		// Choose the velocity.
+		var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+		mob.LinearVelocity = velocity.Rotated(direction);
+
+		// Spawn the mob by adding it to the Main scene.
+		AddChild(mob);
+	}
+	
+	// We also specified this function name in PascalCase in the editor's connection window.
+	private void OnScoreTimerTimeout()
+	{
+		_score++;
+		GetNode<Hud>("HUD").UpdateScore(_score);
+	}
+
+	// We also specified this function name in PascalCase in the editor's connection window.
+	private void OnStartTimerTimeout()
+	{
+		GetNode<Timer>("MobTimer").Start();
+		GetNode<Timer>("ScoreTimer").Start();
+	}
+}
